e7d45e92c2651e326319838367ce6f275471c5d6
Andrew Roberts <aroberts@broadinstitute.org>
2011-12-22 16:41:45 -0500

    deploy.rollback for fabric

--- deploy.py
+++ deploy.py
@@ -6,13 +6,13 @@ from config.fabric.helpers import remote
 
 @task(default=True)
 def update(upgrade_requirements=False):
-    '''Deploy a new version'''
+    '''Deploy a new version from origin/master.'''
     from config.fabric import uwsgi
 
     authenticate()
     checkout_source()
     install_config()
-    install_requirements(upgrade_requirements)
+    install_requirements(upgrade=upgrade_requirements)
     make_symlinks()
     if exists(env.uwsgi_pidfile):
         uwsgi.reload()
@@ -38,6 +38,8 @@ def cold():
 
 @task
 def from_workspace():
+    '''Deploy a new version from this working copy.  Not for use unless
+    circumstances require.'''
     local("gnutar -czf %(release_name)s.tar *" % env)
     with prefix("umask 0002"):
         remote("mkdir %(release_path)s" % env)
@@ -52,6 +54,39 @@ def from_workspace():
     make_symlinks()
     make_workspace_file()
 
+@task
+def rollback():
+    '''Rolls back the install to the most recent release that is prior to
+    the current release.'''
+    from config.fabric import uwsgi
+
+    uwsgi.stop()
+    target = os.path.join(env.releases_path, find_previous_release())
+    install_config(release_path=target)
+    install_requirements(release_path=target)
+    make_symlinks(release_path=target)
+    uwsgi.start()
+
+def find_previous_release():
+    with settings(hide('stdout')):
+        releases = run("ls -1 %(releases_path)s" % env)
+
+    releases = sorted(releases.split())
+    current_index = releases.index(find_canonical_current_release())
+    if current_index < 1:
+        raise Exception("Current release is the oldest.")
+
+    return releases[current_index-1]
+
+def find_canonical_current_release():
+    with settings(hide('stdout')):
+        with cd(env.current_path):
+            current = os.path.basename(run("pwd -P"))
+
+    return current
+
+def clean_requirements():
+    pass # TODO: implement clean_requirements - fresh venv somehow
 
 def setup_virtualenv():
     virtualenv_cmd = ["virtualenv",
@@ -85,7 +120,7 @@ def checkout_source():
         run("rm -rf %s" % os.path.join(env.release_path, ".git"))
 
 
-def install_requirements(upgrade=False):
+def install_requirements(release_path=env.release_path, upgrade=False):
     '''Install requirements into pip from config/requirements.pip'''
     command = []
     command.append("pip")
@@ -96,21 +131,23 @@ def install_requirements(upgrade=False):
 
     with prefix("umask 0002"):
         with prefix(env.activate_virtualenv):
-            with cd(env.release_path):
+            with cd(release_path):
                 remote(" ".join(command))
 
 
-def make_symlinks():
+def make_symlinks(release_path=env.release_path):
     '''Create a 'current' symlink pointing to a release we just checked 
     out, and symlinks within pointing to the shared children'''
     with settings(hide('warnings'), warn_only=True):
         remote("test -L %(current_path)s && rm %(current_path)s" % env)
 
-    remote("ln -s %(release_path)s %(current_path)s" % env)
+    remote("ln -s %s %s" % (release_path, env.current_path))
     for child in env.shared_children:
+        child_path = os.path.join(release_path, child)
+        remote("test -L %s && rm %s" % (child_path, child_path))
         remote("ln -s %s %s" % (
             os.path.join(env.shared_path, child),
-            os.path.join(env.release_path, child)
+            child_path
         ))
         
 
@@ -126,13 +163,14 @@ def make_workspace_file():
                                                    env.release_name)
     remote("echo \"%s\" > %s" % (ws_string, ws_file))
     
-def install_config():
-    config_dir = os.path.join(env.release_path, "config")
-
-    remote("ln -s %(production)s %(local)s" % {
+def install_config(release_path=env.release_path):
+    config_dir = os.path.join(release_path, "config")
+    paths = {
         "production": os.path.join(config_dir, "production.py"),
         "local": os.path.join(config_dir, "local.py")
-    })
+    }
+    remote("test -L %(local)s && rm %(local)s" % paths)
+    remote("ln -s %(production)s %(local)s" % paths)
 
 def authenticate():
     with settings(hide('running')):
