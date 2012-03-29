aa7e2e2c507948233769dd31fac2b8d7b53eec92
Andrew Roberts <aroberts@broadinstitute.org>
2012-03-29 17:32:10 -0400

    added 'tasks' module, for general tasks.  Added a task to print out rm statements (but not execute) for old releases

---
+++ tasks.py
@@ -0,0 +1,19 @@
+import os
+import time
+
+from fabric.api import task, env, prefix, cd, run, settings, hide
+
+@task
+def old_releases(days=60):
+    interval = days * 24 * 60 * 60
+
+    cutoff = time.strftime(env.release_time_format, 
+                           time.localtime(time.time() - interval))
+
+    with cd(env.releases_path):
+        with settings(hide('stdout')):
+            result = run("ls -1")
+
+    for line in result.split("\n"):
+        if line < cutoff:
+            print "rm -rf %s" % os.path.join(env.releases_path, line)
