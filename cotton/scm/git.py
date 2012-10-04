nygenome/cotton

commit a07631d0cf5106c26a75ef9eb62713a96af3b530

added support for deploying tags with cotton

cotton/scm/git.py
@@ -9,27 +9,37 @@ class Git(SCM):
         commands = ' '.join(['git'] + list(commands))
         fab.run(commands)
 
-    def checkout(self, repository, checkout_to, branch=None):
+    def checkout(self, repository, checkout_to, ref=None):
+
         self.git('clone', repository, checkout_to)
-        if branch:
+        if ref:
             with fab.cd(checkout_to):
-                self.git('checkout -b deploy', "origin/%s" % branch)
+                self.git('checkout', ref)
 
 
+    def branch_name(self, repository, output_file=None, append=False):
+        return self._log_state(command=['status | grep "On branch"']
+                               repository=repository,
+                               output_file=output_file,
+                               append=append)
 
-    def branch_name(self, checkout_to, output_file=None, append=False):
-        command = ['status | grep "On branch"']
-        with fab.cd(checkout_to):
-            if output_file:
-                redirect = '>'
-                if append:
-                    redirect = '>>'
-                command.extend([redirect, output_file])
-            return self.git(*command)
+    def revision(self, repository, output_file=None, append=False):
+        return self._log_state(command=['rev-parse HEAD']
+                               repository=repository,
+                               output_file=output_file,
+                               append=append)
 
-    def revision(self, checkout_to, output_file=None, append=False):
-        command = ['rev-parse HEAD']
-        with fab.cd(checkout_to):
+
+    def tag_name(self, repository, output_file=None, append=False):
+        with fab.settings(fab.hide('warnings'), warn_only=True):
+            return self._log_state(command=['describe --tags 2>/dev/null'],
+                                   repository=repository,
+                                   output_file=output_file,
+                                   append=append)
+
+   
+    def _log_state(self, command, repository, output_file=None, append=False):
+        with fab.cd(repository):
             if output_file:
                 redirect = '>'
                 if append:

cotton/deploy.py
@@ -118,17 +118,18 @@ def make_shared_children_dirs():
             helpers.remote("test -d %s || mkdir %s" % (child, child))
 
 
-def checkout_source(branch=None):
+def checkout_source(ref=None):
     # TODO: cached copy strategy
     # TODO: submodules
     scm = env.scm_tool
     with fab.prefix("umask 0002"):
-        scm.checkout(env.scm_repository, env.release_path, branch)
+        scm.checkout(env.scm_repository, env.release_path, ref)
         # bug in early 1.7 git versions, fixed 2012-07-15 - git update needed
         fab.run("chmod g+w %(release_path)s" % env)
 
         revision_path = os.path.join(env.release_path, "REVISION")
         scm.branch_name(env.release_path, revision_path)
+        scm.tag_name(env.release_path, revision_path, append=True)
         scm.revision(env.release_path, revision_path, append=True)
         fab.run("rm -rf %s" % os.path.join(env.release_path, ".git"))
 

commit d9a121bc4362f308b1eccb4f32859490f9d49416

fix syntax error introduced in a07631d

cotton/scm/git.py
@@ -18,13 +18,13 @@ class Git(SCM):
 
 
     def branch_name(self, repository, output_file=None, append=False):
-        return self._log_state(command=['status | grep "On branch"']
+        return self._log_state(command=['status | grep "On branch"'],
                                repository=repository,
                                output_file=output_file,
                                append=append)
 
     def revision(self, repository, output_file=None, append=False):
-        return self._log_state(command=['rev-parse HEAD']
+        return self._log_state(command=['rev-parse HEAD'],
                                repository=repository,
                                output_file=output_file,
                                append=append)