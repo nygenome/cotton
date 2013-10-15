ee228e2f0e3fdc56b3fed8e86cd08a382b0a92fa
Clinton Howarth <chowarth@broadinstitute.org>
2013-10-15 16:31:36 -0400

    OLIVE-517 add fabric task to facilitate application alert message

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='cotton',
-    version='1.0.3',
+    version='1.0.4',
     packages=['cotton',],
     license='BSD',
     long_description=open('README.markdown').read(),
