8282cafeddb6ca71f94f777736e556756e477b59
Andrew Roberts <adroberts@gmail.com>
2014-02-28 11:08:15 -0500

    issues with git tag, bumping again

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='cotton',
-    version='1.1.1',
+    version='1.1.2',
     packages=['cotton',],
     license='BSD',
     long_description=open('README.markdown').read(),
