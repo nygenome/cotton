c840653295761160d34b17dd239e02f8ef96ad36
Andrew Roberts <adroberts@gmail.com>
2014-02-28 10:25:48 -0500

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='cotton',
-    version='1.1.0',
+    version='1.1.1',
     packages=['cotton',],
     license='BSD',
     long_description=open('README.markdown').read(),
