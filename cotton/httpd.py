d2ab938d18a0ba49aaebdb76a5068e963bc3a926
Janet Gainer-Dewar <jgainerdewar@nygenome.org>
2015-06-23 15:26:00 -0400

    Add Apache support

---
+++ cotton/httpd.py
@@ -0,0 +1,66 @@
+import os
+
+from fabric import api as fab
+from fabric.api import env
+
+from fabric.contrib.files import exists
+from fabric.contrib.files import upload_template
+
+from cotton import helpers
+from cotton import set_env, register_setup
+
+"""
+These tasks allow control of an Apache process. We assume your server is
+running Apache through the newer systemctl control scheme.
+"""
+
+def setup(**overrides):
+    set_env("httpd_init_script", "systemctl", **overrides)
+    set_env("httpd_service_name", "httpd.service", **overrides)
+    set_env("httpd_conf_template", os.path.join("config", "servers", "httpd.template.conf"), **overrides)
+    set_env("httpd_conf_dir", os.path.join(env.servers_path, "httpd"), **overrides)
+    set_env("httpd_conf", os.path.join(env.httpd_conf_dir, "httpd.conf"), **overrides)
+register_setup(setup)
+
+@fab.task
+def start(sudo=True):
+    '''Start the httpd instance.'''
+    httpd_cmd('start', sudo)
+
+@fab.task
+def stop(sudo=True):
+    '''Stop the httpd instance.'''
+    httpd_cmd('stop', sudo)
+
+@fab.task
+def restart(sudo=True):
+    '''Stop the httpd instance.'''
+    httpd_cmd('restart', sudo)
+
+@fab.task
+def reload(sudo=True):
+    '''Reload the httpd instance.'''
+    httpd_cmd('reload', sudo)
+
+@fab.task
+def status(sudo=True):
+    '''Print the current status of httpd.'''
+    httpd_cmd('status', sudo)
+
+@fab.task
+def update_conf(sudo=True, reload=False):
+    '''Regenerate and install the httpd config from the template.'''
+    with fab.prefix("umask 0002"):
+        upload_template(env.httpd_conf_template,
+                        env.httpd_conf,
+                        context=env,
+                        mode=0664)
+
+    if reload:
+        reload(sudo)
+
+def httpd_cmd(cmd, sudo):
+    c = [ env.httpd_init_script, cmd, env.httpd_service_name ]
+    if sudo:
+        c.insert(0, 'sudo')
+    helpers.remote(' '.join(c))
