befae4f092975f5acc12efc4b97e2648ed832591
Janet Gainer-Dewar <jgainerdewar@nygenome.org>
2015-10-21 14:00:04 -0400

    Allow configuration of pip's index. Also allow installation from file other than configured requirements file.

--- cotton/pip.py
+++ cotton/pip.py
@@ -4,6 +4,11 @@ from fabric import api as fab
 from fabric.api import env
 
 from cotton import helpers
+from cotton import set_env, register_setup
+
+def setup(**overrides):
+    set_env("pip_index", None, **overrides)
+register_setup(setup)
 
 @fab.task
 def freeze():
@@ -11,7 +16,11 @@ def freeze():
 
 @fab.task
 def install(package):
-    pip("install %s" % package)
+    if env.pip_index is None:
+        cmd = "install "
+    else:
+        cmd = "install -i %s" % env.pip_index
+    pip("%s %s" % (cmd, package))
 
 @fab.task
 def uninstall(package):
@@ -34,10 +43,19 @@ def install_requirements(release_path, *command_line_flags):
     Install requirements into the virtualenv from the file specified in
     `env.requirements_file`
     '''
+    install_from_file(release_path, env.requirements_file, 
+                      *command_line_flags)
+
+def install_from_file(release_path, filepath, *command_line_flags):
+    '''
+    Install requirements into the virtualenv from the file specified.
+    '''
     command = []
     command.append("install")
     command.extend(command_line_flags)
-    command.append("-r %s" % env.requirements_file)
+    if env.pip_index is not None:
+        command.extend(['-i', env.pip_index])
+    command.append("-r %s" % filepath)
 
     with fab.cd(release_path):
         pip(' '.join(command))
